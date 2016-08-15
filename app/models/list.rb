c5fec53e33bb603e44f772be948e8bff50f63dd4
Kate Walters <kawalters01@gmail.com>
2016-08-15 10:17:55 -0500

    Added model validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,10 @@
 class List < ApplicationRecord
+  belongs_to :user
+  has_many :tasks
+
+  validates :title, length: { minimum: 2, maximum: 200 }
+  validates :difficulty, presence: true
+  validates :energy, presence: true
+  validates :completed, default: false, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :lists
+  has_many :tasks, through: :lists
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
