43169ee57fcc988d0292f804b372542a33ecdbae
Kate Walters <kawalters01@gmail.com>
2016-08-26 15:54:44 -0500

    Added space between delete button and task item

--- app/assets/javascripts/components/child_strikethrough.js.jsx
+++ app/assets/javascripts/components/child_strikethrough.js.jsx
@@ -18,7 +18,8 @@ var ChildStrikethrough = React.createClass({
     return (
       <li>
         <span className={this.state.isStrikeThrough} onClick={this.onHandleClick}>{this.props.child.title}</span>
-        <a className="" href={"/tasks/new?parent_task_id="}>
+
+      <a className="react-clear" href={"/tasks/new?parent_task_id="}>
         <i className="tiny material-icons">clear</i></a>
       </li>
     )
